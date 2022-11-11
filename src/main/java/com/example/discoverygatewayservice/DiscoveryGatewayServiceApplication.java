7c54364f4a8f083f7b6be4043b1dd7b0b9647f7b
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-11-11 19:45:23 +0300

    Initial commit

---
+++ src/main/java/com/example/discoverygatewayservice/DiscoveryGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoverygatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discoverygatewayservice/DiscoveryGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoverygatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
