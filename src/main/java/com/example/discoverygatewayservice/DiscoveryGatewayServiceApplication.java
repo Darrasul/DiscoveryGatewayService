aa25759887f37bb698e09f6cd3f931ee81791e0d
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-11-11 19:53:00 +0300

    Done homework at 19:52 MSK 11.11.22

--- src/main/java/com/example/discoverygatewayservice/DiscoveryGatewayServiceApplication.java
+++ src/main/java/com/example/discoverygatewayservice/DiscoveryGatewayServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.discoverygatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DiscoveryGatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryGatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryGatewayServiceApplication.class, args);
+    }
 
 }
